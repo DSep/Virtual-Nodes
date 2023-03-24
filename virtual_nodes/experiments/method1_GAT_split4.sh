07c004732cf028c077d3573f4af5a768a0f64fea
DSep <31024588+DSep@users.noreply.github.com>
2023-03-24 02:26:12 +0000

    Split final runners

---
+++ virtual_nodes/experiments/method1_GAT_split4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# Variables
+GENERIC_PARAM="--splits 3 --n_seeds 3 --epochs 1500 --patience 100 --verbosity 0 --augment --augment_ratio 1.0 --include_vnode_labels --clip --khops 2"
+LAYER_2="--layer 2"
+
+# Operations
+cd Heterophily_and_oversmoothing
+
+echo "EXPERIMENT SET: ${GENERIC_PARAM} ${LAYER_2}"
+# GAT 
+python -u full-supervised.py --data texas       $GENERIC_PARAM $k $LAYER_2 --weight_decay 5e-5 --model GAT --hidden 8 --dropout 0.6 --lr 0.005             
+python -u full-supervised.py --data wisconsin   $GENERIC_PARAM $k $LAYER_2 --weight_decay 5e-5 --model GAT --hidden 8 --dropout 0.6 --lr 0.005             
+python -u full-supervised.py --data film        $GENERIC_PARAM $k $LAYER_2 --weight_decay 5e-5 --model GAT --hidden 8 --dropout 0.6 --lr 0.005 --use_sparse
+python -u full-supervised.py --data squirrel    $GENERIC_PARAM $k $LAYER_2 --weight_decay 5e-5 --model GAT --hidden 8 --dropout 0.6 --lr 0.005 --use_sparse
+python -u full-supervised.py --data pubmed      $GENERIC_PARAM $k $LAYER_2 --weight_decay 5e-5 --model GAT --hidden 8 --dropout 0.6 --lr 0.005 --use_sparse
+
+
--- virtual_nodes/experiments/method1_GCN.sh
+++ virtual_nodes/experiments/method1_GCN.sh
@@ -14,14 +14,14 @@ for k in "${khops[@]}"
 do
   echo "EXPERIMENT SET: ${GENERIC_PARAM} ${k} ${LAYER_2}"
   # GCN
-  # python -u full-supervised.py --data cora        $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-4 --model GCN --hidden 16 
-  # python -u full-supervised.py --data citeseer    $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-4 --model GCN --hidden 16 
-  # python -u full-supervised.py --data pubmed      $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
+  python -u full-supervised.py --data cora        $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-4 --model GCN --hidden 16 
+  python -u full-supervised.py --data citeseer    $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-4 --model GCN --hidden 16 
+  python -u full-supervised.py --data pubmed      $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
   python -u full-supervised.py --data chameleon   $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16
-  # python -u full-supervised.py --data cornell     $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
-  # python -u full-supervised.py --data texas       $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
-  # python -u full-supervised.py --data wisconsin   $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
-  # python -u full-supervised.py --data squirrel    $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
-  # python -u full-supervised.py --data film        $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
+  python -u full-supervised.py --data cornell     $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
+  python -u full-supervised.py --data texas       $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
+  python -u full-supervised.py --data wisconsin   $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
+  python -u full-supervised.py --data squirrel    $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
+  python -u full-supervised.py --data film        $GENERIC_PARAM $k $d $LAYER_2 --weight_decay 5e-5 --model GCN --hidden 16 
 done
 
